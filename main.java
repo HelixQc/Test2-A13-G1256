8c7daca16b706ef56c738bde7bfc91281427738f
HelixQc <156359638+HelixQc@users.noreply.github.com>
2024-01-11 16:34:28 -0500

    New message in code main

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello word!! MickeyMouse is ready to code");
+        System.out.println("say hello to my little friend");
+    }
+}
